ac91b794fa36fb7a46b6f1e727760d54e1b7c2ac
Raj Narayan Singh <rs902869@gmail.com>
2024-07-14 18:06:02 +0545

    passowrd fn

---
+++ day121function.py
@@ -0,0 +1,16 @@
+# def displayname():
+#     print('My name is RNS')
+# displayname()
+
+
+# def displayphone_no():
+#     print('My phone no  is 28w6725w7618')
+# displayphone_no()
+
+# def display_even(first,last):
+#     for i in range(first,last+1):
+#         if i%2==0:
+#            print(i)
+# display_even(10,80)
+
+
---
+++ day12function.py
@@ -0,0 +1,22 @@
+# for i in range(1,101):
+#     print(i)
+
+
+# for i in range(1,10):
+#     if i==4:
+#         break
+#     print(i)
+
+
+# for i in range(1,11):
+#     if i==4:
+#         continue
+#     print(i)
+
+
+for i in  range(70,101):
+    if i==77 or i==78 or i==81:
+       continue
+    print(i)
+
+
